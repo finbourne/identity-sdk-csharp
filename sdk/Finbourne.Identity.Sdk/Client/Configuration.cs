b4fd3ed3cb78f5ec7d81c65496ffe260c855a2d4
Concourse <concourse@finbourne.com>
2024-05-08 12:32:07 +0000

    Updates for v2.1.9

--- sdk/Finbourne.Identity.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Identity.Sdk/Client/Configuration.cs
@@ -544,7 +544,7 @@ namespace Finbourne.Identity.Sdk.Client
             string report = "C# SDK (Finbourne.Identity.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.0.2898\n";
+            report += "    Version of the API: 0.0.2900\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
