04ee306db30104e81cda4f35cf0b064220303fa4
Concourse <concourse@finbourne.com>
2024-08-29 16:52:00 +0000

    identity csharp updates for v2.1.57
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/Finbourne.Identity.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Identity.Sdk/Client/Configuration.cs
@@ -544,7 +544,7 @@ namespace Finbourne.Identity.Sdk.Client
             string report = "C# SDK (Finbourne.Identity.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.0.3022\n";
+            report += "    Version of the API: 0.0.3024\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
